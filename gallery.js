cf168218d6f6bd8c7311e035d4ea592e2eff4705
rofe <github@rofe.com>
2021-03-07 22:00:49 +0100

    refactor: wip

--- gallery.js
+++ gallery.js
@@ -3,32 +3,41 @@ function link($elem, url) {
     'href': url,
   });
   $elem.parentNode.insertBefore($link, $elem);
-  $link.appendChildNode($elem);
+  $link.appendChild($elem);
+  return $link;
 }
 
-// wrap thumbnails with links to the full version
-const sections = Array.from(document.querySelectorAll('main > div'));
-const hasHeader = sections[0].classList.contains('hero');
-const $gallerySection = hasHeader ? sections[1] : sections[0];
-const $gallery = $gallerySection.querySelector('div');
-const pics = Array.from($gallery.querySelectorAll('picture')).forEach(($pic) => {
-  const thumbUrl = $pic.firstElementChild.getAttribute('src');
-  const fullUrl = thumbUrl.replace('width=374', 'width=2000');
-  link($pic, fullUrl);
-});
-$gallery.classList.add('gallery');
-$gallery.querySelectorAll('p:empty').forEach(($p) => $p.remove());
-if (hasHeader) {
-  // add gallery below title inside header section
-  sections[0].appendChild($gallerySection);
-}
+window.addEventListener('DOMContentLoaded', () => {
+  window.gallery.observer.disconnect();
 
-const $finalSection = sections.pop();
-if ($finalSection !== $gallerySection) {
-  $finalSection.classList.add('copyright');
-}
+  // wrap thumbnails with links to the full version
+  const sections = Array.from(document.querySelectorAll('main > div'));
+  const hasHeader = sections[0] && sections[0].classList.contains('hero');
+  const $gallerySection = hasHeader ? sections[1] : sections[0];
+  const $gallery = $gallerySection.querySelector('div') || $gallerySection;
+  $gallery.querySelectorAll('picture').forEach(($pic) => {
+    const img = $pic.querySelector('img');
+    console.log(img.src);
+    return;
+    const thumbUrl = img.getAttribute('src');
+    const fullUrl = thumbUrl.replace('width=374', 'width=2000');
+    const thumb = link(img, fullUrl);
+    $pic.parentElement.appendChild(thumb);
+    $pic.remove();
+  });
+  $gallery.classList.add('gallery');
+  $gallery.querySelectorAll('p:empty').forEach(($p) => $p.remove());
+  if (hasHeader) {
+    // add gallery below title inside header section
+    sections[0].appendChild($gallerySection);
+  }
 
-window.gallery.observer.disconnect();
-lightGallery($gallery, {
-  thumbnail: true,
+  const $finalSection = sections.pop();
+  if ($finalSection !== $gallerySection) {
+    $finalSection.classList.add('addendum');
+  }
+
+  // lightGallery($gallery, {
+  //   thumbnail: true,
+  // });
 });
