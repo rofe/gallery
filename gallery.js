69e61e90335da97861deeefadda4c54c76a5b0d4
rofe <github@rofe.com>
2021-03-07 23:20:31 +0100

    refactor: wip

--- gallery.js
+++ gallery.js
@@ -8,36 +8,32 @@ function link($elem, url) {
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-  window.gallery.observer.disconnect();
+  // window.gallery.observer.disconnect();
 
   // wrap thumbnails with links to the full version
-  const sections = Array.from(document.querySelectorAll('main > div'));
+  const sections = Array.from(document.querySelectorAll('.section-wrapper'));
   const hasHeader = sections[0] && sections[0].classList.contains('hero');
-  const $gallerySection = hasHeader ? sections[1] : sections[0];
-  const $gallery = $gallerySection.querySelector('div') || $gallerySection;
+  const $gallery = document.querySelector('.gallery');
+  const $gallerySection = $gallery.closest('.section-wrapper');
   $gallery.querySelectorAll('picture').forEach(($pic) => {
-    const img = $pic.querySelector('img');
-    console.log(img.src);
-    return;
-    const thumbUrl = img.getAttribute('src');
+    const $img = $pic.querySelector('img');
+    const thumbUrl = $img.getAttribute('src');
     const fullUrl = thumbUrl.replace('width=374', 'width=2000');
-    const thumb = link(img, fullUrl);
-    $pic.parentElement.appendChild(thumb);
+    const $thumb = link($img, fullUrl);
+    $pic.parentElement.appendChild($thumb);
     $pic.remove();
   });
-  $gallery.classList.add('gallery');
-  $gallery.querySelectorAll('p:empty').forEach(($p) => $p.remove());
   if (hasHeader) {
     // add gallery below title inside header section
-    sections[0].appendChild($gallerySection);
+    sections[0].appendChild($gallery);
   }
 
-  const $finalSection = sections.pop();
-  if ($finalSection !== $gallerySection) {
-    $finalSection.classList.add('addendum');
+  const epilog = sections.pop();
+  if (epilog !== $gallerySection) {
+    epilog.classList.add('epilog');
   }
 
-  // lightGallery($gallery, {
+  // lightGaller($gallery, {
   //   thumbnail: true,
   // });
 });
