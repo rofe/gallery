188815a6696757730987e5a048a34f00ad85f8ae
rofe <github@rofe.com>
2021-03-08 21:18:30 +0100

    feat: implemented lightgallery

--- js/gallery.js
+++ js/gallery.js
@@ -2,6 +2,7 @@ function link($elem, href, cl) {
   const $link = createTag('a', {
     href,
     class: cl,
+    tabindex: 0,
   });
   $elem.parentNode.insertBefore($link, $elem);
   $link.appendChild($elem);
@@ -9,43 +10,42 @@ function link($elem, href, cl) {
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-  // window.gallery.observer.disconnect();
+  window.gallery.observer.disconnect();
 
-  // wrap thumbnails with links to the full version
-  const sections = Array.from(document.querySelectorAll('.section-wrapper'));
-  const hasHeader = sections[0] && sections[0].classList.contains('hero');
   const $gallery = document.querySelector('.gallery');
   if (!$gallery) {
     return;
   }
+
+  // wrap thumbnails with links to the full version
   const $gallerySection = $gallery.closest('.section-wrapper');
   $gallery.querySelectorAll('picture').forEach(($pic) => {
     const $img = $pic.querySelector('img');
-    const thumbUrl = $img.getAttribute('src');
-    const fullUrl = thumbUrl
-      .replace(/\?width\=\d+/, '?width=2000')
-      .replace(/\&crop\=\d+:\d+/,'');
+    const fullUrl = $img.src
+      .replace(/\?width\=\d+/, '?width=2000') // increase width to 2000px
+      .replace(/\&crop\=\d+:\d+/,''); // remove cropping
     const $thumb = link($img, fullUrl, 'thumb');
     $pic.parentElement.appendChild($thumb);
     $pic.remove();
   });
+  
+  const sections = Array.from(document.querySelectorAll('.section-wrapper'));
+  const hasHeader = sections[0] && sections[0].classList.contains('hero');
   if (hasHeader) {
-    // add gallery below title inside header section
+    // add gallery below title if header section
     sections[0].appendChild($gallery);
   }
 
-  const epilog = sections.pop();
-  if (epilog !== $gallerySection) {
-    epilog.classList.add('epilog');
+  // treat last section as epilog
+  const $epilogSection = sections.pop();
+  if ($epilogSection !== $gallerySection) {
+    $epilogSection.classList.add('epilog');
   }
 
-  const $container = $gallery.querySelector('.gallery > div > div');
-  $container.classList.add('gallery-wall');
-  lightGallery($container, {
+  // find wall div and initialize lightgallery
+  const $wall = $gallery.querySelector('.gallery > div > div');
+  $wall.classList.add('gallery-wall');
+  lightGallery($wall, {
     thumbnail: true,
   });
 });
-
-window.addEventListener('resize', () => {
-
-})
